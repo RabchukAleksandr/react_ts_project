8431a3fcafc59587d70a8674f32d82af156421d6
Rabchuk Oleksandr <rabchuk.alexandr@ukr.net>
2021-01-28 17:53:18 +0200

    init commit

---
+++ src/components/header.tsx
@@ -0,0 +1,24 @@
+import logo from "../img/pizza-logo.svg";
+import React from "react";
+import {Button} from "../components"
+
+const Header =()=>{
+    return(
+        <div className="header">
+            <div className="container">
+                <div className="header__logo">
+                    <img width="38" src={logo} alt="Pizza logo"/>
+                    <div>
+                        <h1>React Pizza</h1>
+                        <p>самая вкусная пицца во вселенной</p>
+                    </div>
+                </div>
+                <div className="header__cart">
+                    <Button className="button--cart" />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Header;
